93095b65abbfa639a16eb4fc3ac9de30f4716bf9
Milan Dinić <forumi@srpski.biz>
2016-08-19 20:30:45 +0200

    Disable navbar colapse.

--- index.php
+++ index.php
@@ -11,6 +11,24 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<?php wp_head(); ?>
+		<?php // http://stackoverflow.com/a/23536146 ?>
+		<style type="text/css">
+		.navbar-collapse.collapse {
+		  display: block!important;
+		}
+
+		.navbar-nav>li, .navbar-nav {
+		  float: left !important;
+		}
+
+		.navbar-nav.navbar-right:last-child {
+		  margin-right: -15px !important;
+		}
+
+		.navbar-right {
+		  float: right!important;
+		}
+		</style>
 	</head>
 
 	<body <?php body_class(); ?>>
